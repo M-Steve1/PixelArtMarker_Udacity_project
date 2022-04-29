8bc9408693e4f84a35a370498165886d14db2486
Modebe Stephen <modebestephen2@gmail.com>
2022-04-29 01:05:29 +0100

    Declare variables

---
+++ designs.js
@@ -0,0 +1,6 @@
+const myTable = document.getElementById('pixelCanvas');
+const submitBtn = document.getElementById('inputSubmit');
+const colorPickerBtn = document.getElementById('colorPicker');
+const myForm = document.getElementById('sizePicker');
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
